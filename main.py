6adb5d4f3fef44185869dedb50521535c66bd64c
Romeu Vieira <romeu.bizz@gmail.com>
2020-02-11 22:32:51 +0000

    Create dir from xml file name // Create output directory

--- main.py
+++ main.py
@@ -4,15 +4,35 @@ import os
 
 import xml.etree.ElementTree as ET
 
+output_dir = 'output'
+
 def get_feed_files():
     return glob.glob('./feeds/*.xml')
 
-def parse_xml(xml_file):
-    for file in xml_file:
+def get_file_name(path):
+    return path.split('/')[-1][:-4]
+
+def parse_xml(xml_files):
+    for file in xml_files:
+
+        file_name = get_file_name(file)
+        path = 'output/' + file_name
+
+        try:
+            os.mkdir('output/' + file_name)
+        except OSError:
+            print(f"Directory {path} already exists.")
+
         tree = ET.parse(file)
         root = tree.getroot()
-        print(root[0])
+        #print(root[0].text)
     return
 
+try:
+    os.mkdir(output_dir)
+except OSError:
+    print(f"Directory {output_dir} already exists.")
+
 feed_files = get_feed_files()
+feed_files.sort()
 parse_xml(feed_files)
